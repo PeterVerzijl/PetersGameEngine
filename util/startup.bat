4395d328c470f8b32d902275da5e4785245a7bc4
Peter Verzijl <p.verzijl@student.utwente.nl>
2015-12-19 21:49:41 +0100

    Fixed the startup batch file. Added link to cmd that calls the startup batch on startup for convenience.

--- util/startup.bat
+++ util/startup.bat
@@ -1,7 +1,7 @@
 @echo off
 
+call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 SUBST G: /D
 subst G: C:\Users\User\Documents\GitHub\PetersGameEngine
 G:
 cd \code
-call "c:\Program Files (x86)\Visual Studio 14.0\VC\vcvarsall.bat" x64
