042a7cc0567c0551561a9d2d816c13fe1dd6ea35
Peter Verzijl <peter@verzijl.com>
2016-03-04 01:58:36 +0100

    Added structures for the world and bitmap loading. Removed unused code.

--- code/game.h
+++ code/game.h
@@ -1,11 +1,11 @@
-/**************************************************************************** 
-* File:			game.h
-* Version:		0.0.1a
-* Date:			27-12-2015
-* Creator:		Peter Verzijl
-* Description:	Game Layer Header File
-* Notice:		(c) Copyright 2015 by Peter Verzijl. All Rights Reserved.
-***************************************************************************/
+	/**************************************************************************** 
+	* File:			game.h
+	* Version:		0.0.1a
+	* Date:			27-12-2015
+	* Creator:		Peter Verzijl
+	* Description:	Game Layer Header File
+	* Notice:		(c) Copyright 2015 by Peter Verzijl. All Rights Reserved.
+	***************************************************************************/
 /*
 	NOTE:
 	INTERNAL
@@ -19,6 +19,10 @@
 
 // TODO(Peter): Implement math functions ourselves
 #include <math.h>
+
+	//
+// NOTE(Peter): Types
+//
 #include <stdint.h>
 
 #define internal static
@@ -39,6 +43,8 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
+typedef size_t memory_index;
+
 typedef float real32;
 typedef double real64;
 
@@ -56,8 +62,9 @@ typedef double real64;
 
 #define ArrayCount(Array)  (sizeof(Array) / sizeof((Array)[0]))
 
-inline uint32
-SafeTruncateInt64(uint64 Value)
+#include "intrinsics.h"
+
+inline uint32 SafeTruncateInt64(uint64 Value)
 {
 	// TODO (peter) : Define max values uint32 etc.
 		Assert(Value <= 0xFFFFFFFF);	// Never read files bigger then 4 gigabytes!
@@ -65,29 +72,36 @@ SafeTruncateInt64(uint64 Value)
 	return(Result);
 }
 
+struct thread_context
+{
+	int Placeholder;
+};
+
 /*
-Services that the platform layer provides to the game
+	Services that the platform layer provides to the game
 */
 #if INTERNAL
-struct debug_file_result 
+// IMPORTANT(Peter): These are not for shipping code! 
+// They are blocking and do not protect against lost data.
+struct debug_read_file_result 
 {
 	uint32 ContentSize;
 	void *Content;
 };
 // NOTE(Peter): These get passed to the game via the game_memory struct.
-#define DEBUG_PLATFORM_FREE_FILE_MEMORY(name) void name(void *Memory)
+#define DEBUG_PLATFORM_FREE_FILE_MEMORY(name) void name(thread_context *Thread, void *Memory)
 typedef DEBUG_PLATFORM_FREE_FILE_MEMORY(debug_platform_free_file_memory);
 
-#define DEBUG_PLATFORM_READ_ENTIRE_FILE(name) debug_file_result name(char *Filename)
+#define DEBUG_PLATFORM_READ_ENTIRE_FILE(name) debug_read_file_result name(thread_context *Thread, char *Filename)
 typedef DEBUG_PLATFORM_READ_ENTIRE_FILE(debug_platform_read_entire_file);
 
-#define DEBUG_PLATFORM_WRITE_ENTIRE_FILE(name) bool32 name(char *Filename, uint32 MemorySize, void *Memory)
+#define DEBUG_PLATFORM_WRITE_ENTIRE_FILE(name) bool32 name(thread_context *Thread,  char *Filename, uint32 MemorySize, void *Memory)
 typedef DEBUG_PLATFORM_WRITE_ENTIRE_FILE(debug_platform_write_entire_file);
 
 #endif
 
 /*
-	Services that the game provides to the platform layer
+	NOTE(Peter): Services that the game provides to the platform layer
 */
 struct game_offscreen_buffer
 {
@@ -95,6 +109,7 @@ struct game_offscreen_buffer
 	int Width;
 	int Height;
 	int Pitch;
+	int BytesPerPixel;
 };
 
 struct game_sound_output_buffer
@@ -150,7 +165,11 @@ struct game_controller_input
 
 struct game_input
 {
-	// TODO (Peter) : Add timing here
+	game_button_state MouseButtons[5];
+	int32 MouseX, MouseY, MouseZ;
+
+	real32 DeltaTime;		// Amount of seconds to advance the game by.
+
 	game_controller_input Controllers[5];	// Four controllers and a keyboard on input 0
 };
 inline game_controller_input *GetController(game_input *Input, int ControllerIndex) 
@@ -180,24 +199,47 @@ struct game_clock
 	real32 SecondsElapsed;
 };
 
-#define GAME_UPDATE_AND_RENDER(name) void name(game_memory *Memory, game_offscreen_buffer *Buffer, game_input *Input)
+#define GAME_UPDATE_AND_RENDER(name) void name(thread_context *Thread, game_memory *Memory, game_offscreen_buffer *Buffer, game_input *Input)
 typedef GAME_UPDATE_AND_RENDER(game_update_and_render);
-GAME_UPDATE_AND_RENDER(GameUpdateAndRenderStub)
-{
-}
 
 // NOTE(Peter): This function induces audio latency if it takes longer than 1ms
 // TODO(Peter): Measure the time it takes to do this function.
-#define GAME_GET_SOUND_SAMPLES(name) void name(game_memory *Memory, game_sound_output_buffer *SoundBuffer)
+#define GAME_GET_SOUND_SAMPLES(name) void name(thread_context *Thread, game_memory *Memory, game_sound_output_buffer *SoundBuffer)
 typedef GAME_GET_SOUND_SAMPLES(game_get_sound_samples);
-GAME_GET_SOUND_SAMPLES(GameGetSoundSamplesStub)
+
+struct tile_map
 {
-}
+	int32 CountX;
+	int32 CountY;
+
+	real32 UpperLeftX;
+	real32 UpperLeftY;
+	real32 TileSize;
+	
+	uint32 *Tiles;
+};
+
+struct loaded_bitmap
+{
+	int32 Width;
+	int32 Height;
+	uint32 *Pixels;
+};
+
+struct world
+{
+	// TODO(Peter): Beginners sparseness.
+	int32 TileMapCountX;
+	int32 TileMapCountY;
+
+	tile_map *TileMaps;
+};
 
 struct game_state
 {
-	int ToneHz;
-	int xOffset;
-	int yOffset;
+	real32 PlayerX;
+	real32 PlayerY;
+
 	real32 tSine;
+	loaded_bitmap TestImage;
 };
